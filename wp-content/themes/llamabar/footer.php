fd7889589d0751d19c1fe74720623a143519bfa9
Iaan van Niekerk <iaan@plusplusminus.co.za>
2015-08-06 16:15:30 +0200

    retina and modal fix

--- wp-content/themes/llamabar/footer.php
+++ wp-content/themes/llamabar/footer.php
@@ -19,8 +19,8 @@
 							<div class="flag three"></div>
 						</div>
 						<div class="dont-miss">
-							<a href="#" title="SIGN UP">SIGN UP</a> <span>AND</span> 
-							<p><i class="fa fa-envelope-o"></i> DON’T MISS OUT</p>
+							<a href="#" data-toggle="modal" data-target="#newsletterModal" title="SIGN UP">SIGN UP <span>AND</span> 
+							<p><i class="fa fa-envelope-o"></i> DON’T MISS OUT</p></a>
 						</div>
 					</div>
 				  </div>
@@ -28,6 +28,25 @@
 		  	</div>
 		</div>
   </footer>
+
+  <!-- Modal -->
+  <div class="modal fade" id="newsletterModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <h4 class="modal-title" id="myModalLabel">Subscribe to our Newsletter</h4>
+        </div>
+        <div class="modal-body">
+          <p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Sed posuere consectetur est at lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+
+Maecenas sed diam eget risus varius blandit sit amet non magna. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Aenean lacinia bibendum nulla sed consectetur. Nullam id dolor id nibh ultricies vehicula ut id elit. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+
+Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam id dolor id nibh ultricies vehicula ut id elit. Vestibulum id ligula porta felis euismod semper. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur blandit tempus porttitor. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.</p>
+        </div>
+      </div>
+    </div>
+  </div>
 
 	<!-- all js scripts are loaded in library/bones.php -->
 	<?php wp_footer(); ?>
