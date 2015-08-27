fcd7c850e187fb8ab63970d4d0a0b21d042b0621
Leana10 <leana@plusplusminus.co.za>
2015-08-27 09:11:05 +0200

    404

---
+++ wp-content/themes/llamabar/404.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+<?php global $post; ?>
+
+<section class="vendors">
+	<div class="container">
+
+		<div class="row">
+			<div class="col-md-12">
+				<h1 style="text-align:center;">404</h1>
+			</div>
+			<div class="col-md-12">
+					<p style="text-align:center;">Page Not Found.</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<?php get_footer(); ?>
